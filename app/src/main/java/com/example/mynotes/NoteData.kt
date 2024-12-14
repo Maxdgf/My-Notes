98de1cf3b323cb3affee6cc9807a2382b8c90a6b
Max <162334963+Maxdgf@users.noreply.github.com>
2024-12-14 23:53:20 +0300

    Update NoteData.kt

--- app/src/main/java/com/example/mynotes/NoteData.kt
+++ app/src/main/java/com/example/mynotes/NoteData.kt
@@ -1,4 +1,4 @@
 package com.example.mynotes
 
-class NoteData(val nameNote:String, val idNote:String, val contentNote:String, val isChecked:String, val checkedValue:String, val dateData:String) {
+class NoteData(val nameNote:String, val idNote:String, val contentNote:String, var Checked:String, var checkedValue:String, val dateData:String, val place:Int, val fontName:String, val fontContent:String) {
 }
