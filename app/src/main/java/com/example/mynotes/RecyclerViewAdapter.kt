0fc4667343d93bdc97b4d3167d241118efff6948
Max <162334963+Maxdgf@users.noreply.github.com>
2024-12-15 16:44:31 +0300

    Update RecyclerViewAdapter.kt

--- app/src/main/java/com/example/mynotes/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/mynotes/RecyclerViewAdapter.kt
@@ -5,11 +5,11 @@ import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
 import android.content.Context.CLIPBOARD_SERVICE
+import android.content.SharedPreferences
 import android.graphics.Color
 import android.graphics.Typeface
-import android.os.Bundle
 import android.text.method.ScrollingMovementMethod
-import android.util.SparseBooleanArray
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -24,10 +24,6 @@ import androidx.recyclerview.widget.RecyclerView
 
 class RecyclerViewAdapter(val context:Context, var dataList:ArrayList<NoteData>): RecyclerView.Adapter<RecyclerViewAdapter.noteDataViewHolder>() {
 
-    //attempt for use boolean array to keep checkboxes states
-    //this is a good method, BUT, data always removing after recyclerview updating:((((
-    //private var checkBoxesStates = SparseBooleanArray()
-
     inner class noteDataViewHolder(val view: View): RecyclerView.ViewHolder(view) {
         val noteName = view.findViewById<TextView>(R.id.noteNameView)
         val noteId = view.findViewById<TextView>(R.id.noteIdView)
@@ -36,15 +32,11 @@ class RecyclerViewAdapter(val context:Context, var dataList:ArrayList<NoteData>)
         val btnDeleteNote = view.findViewById<ImageButton>(R.id.deleteThisNote)
         val btnEditNote = view.findViewById<ImageButton>(R.id.editThisNote)
         val btnCopyNote = view.findViewById<ImageButton>(R.id.copyThisNote)
-        var checkedParam = ""
-        var checkValue = ""
         var font1 = ""
         var font2 = ""
+        var align = ""
         var placeIdString = ""
         var placeId = 0
-        val finalCheck = view.findViewById<CheckBox>(R.id.completeChecker)
-        val checkImage = view.findViewById<ImageView>(R.id.checkImageIcon)
-        val icon = view.findViewById<ImageView>(R.id.noteIcon)
         val dateView = view.findViewById<TextView>(R.id.noteDateView)
         val noteNum = view.findViewById<TextView>(R.id.noteNumber)
     }
@@ -55,151 +47,64 @@ class RecyclerViewAdapter(val context:Context, var dataList:ArrayList<NoteData>)
         return noteDataViewHolder(view)
     }
 
+    @SuppressLint("RtlHardcoded")
     override fun onBindViewHolder(holder: noteDataViewHolder, position: Int) {
 
         val dbHelper = DBHelper(context)
-
-        //attempt for fetch and apply checkboxes states from db, because checkboxes always unchecked after i scroll and create new notes
-        /*val database = dbHelper.readableDatabase
-        val projection = arrayOf("Place, CheckValue")
-
-        val cursor = database.query("NotesContentTable", projection, null, null, null, null, null)
-
-        var place: Int? = null
-        var checkValue: String? = null
-
-        var placeList: MutableList<Int> = mutableListOf()
-        var checkList: MutableList<String> = mutableListOf()
-
-        while (cursor.moveToNext()) {
-            place = cursor.getInt(cursor.getColumnIndexOrThrow("Place"))
-            checkValue = cursor.getString(cursor.getColumnIndexOrThrow("CheckValue"))
-
-            checkList = mutableListOf<String>()
-            checkList.add(checkValue)
-
-            placeList = mutableListOf<Int>()
-            placeList.add(place)
-
-            for (place in placeList) {
-                println(place)
-            }
-
-            for (checkValue in checkList) {
-                println(checkValue)
-            }
-
-            checkBoxesStates.put(place, checkValue.toBoolean())
-        }*/
 
         val list = dataList[position]
 
         holder.noteName.text = list.nameNote
         holder.noteId.text = list.idNote
         holder.noteContent.text = list.contentNote
-        holder.checkedParam = list.Checked
-        holder.checkValue = list.checkedValue
+        holder.align = list.textAlign.uppercase()
         holder.dateView.text = list.dateData
         holder.placeId = list.place
         holder.placeIdString = list.place.toString()
         holder.noteNum.text = holder.placeIdString
-        //holder.finalCheck.isChecked = holder.checkValue.toBoolean()
-        //holder.finalCheck.isChecked = list.checkedValue.toBoolean()
-        //val pos = getItemId(position).toInt()
-        //checkBoxesStates.put(pos, holder.checkValue.toBoolean())
-        //val isChecked = checkBoxesStates.get(pos, false)
-        //holder.bindState(checkBoxesStates.get(position))
-        /*if (place != null) {
-            val data = checkBoxesStates.get(place)
-            holder.finalCheck.isChecked = data
-            println("data"+data)
-        }*/
-        //println(checkBoxesStates)
-        //holder.finalCheck.setOnCheckedChangeListener(null)
         holder.font1 = list.fontName.uppercase()
         holder.font2 = list.fontContent.uppercase()
 
-        if (holder.font1 == "MONOSPACE") {
-            holder.noteName.typeface = Typeface.MONOSPACE
-        } else if (holder.font1 == "SERIF") {
-            holder.noteName.typeface = Typeface.SERIF
-        } else if (holder.font1 == "SANS-SERIF") {
-            holder.noteName.typeface = Typeface.SANS_SERIF
-        } else {
-            holder.noteName.typeface = Typeface.SERIF
-        }
-
-        if (holder.font2 == "MONOSPACE") {
-            holder.noteContent.typeface = Typeface.MONOSPACE
-        } else if (holder.font2 == "SERIF") {
-            holder.noteContent.typeface = Typeface.SERIF
-        } else if (holder.font2 == "SANS-SERIF") {
-            holder.noteContent.typeface = Typeface.SANS_SERIF
-        } else {
-            holder.noteContent.typeface = Typeface.SERIF
-        }
-
-        if (holder.checkValue.toBoolean()) {
-            holder.finalCheck.isChecked = true
-            //checkBoxesStates.put(position, true)
-            holder.finalCheck.text = "✓ completed"
-            holder.finalCheck.setBackgroundColor(Color.parseColor("#FF4CAF50"))
-        } else {
-            holder.finalCheck.isChecked = false
-            //checkBoxesStates.put(position, false)
-            holder.finalCheck.text = "✖ not completed"
-            holder.finalCheck.setBackgroundColor(Color.parseColor("#FF0000"))
-        }
-
-        if (holder.checkedParam == "Yes") {
-            holder.finalCheck.visibility = View.VISIBLE
-            holder.checkImage.visibility = View.VISIBLE
-            holder.icon.visibility = View.INVISIBLE
-        } else {
-            holder.finalCheck.visibility = View.INVISIBLE
-            holder.checkImage.visibility = View.INVISIBLE
-            holder.icon.visibility = View.VISIBLE
-        }
-
-        fun checkboxEvent() {
-            val database = dbHelper.writableDatabase
-            val checkIndekator = holder.finalCheck.isChecked
-            val idData = holder.noteId.text
-            val id = idData.drop(4)
-            val name = holder.noteName.text.toString()
-            //val place = holder.noteNum.text.toString()
-            val positiveMessage = "Note '$name' completed."
-            val negativeMessage = "Note '$name' not completed."
-            if (checkIndekator == true) {
-                val query = "UPDATE NotesContentTable SET CheckValue = REPLACE(CheckValue, '${holder.checkValue}', 'true') WHERE Id LIKE '%$id%'"
-                database.execSQL(query)
-                //checkBoxesStates.put(pos, true)
-                //println(position)
-                holder.finalCheck.isChecked = true
-                holder.finalCheck.text = "✓ completed"
-                holder.finalCheck.setBackgroundColor(Color.parseColor("#FF4CAF50"))
-                val toast = Toast.makeText(context, positiveMessage, Toast.LENGTH_SHORT)
-                toast.show()
-                database.close()
-            } else {
-                val query = "UPDATE NotesContentTable SET CheckValue = REPLACE(CheckValue, '${holder.checkValue}', 'false') WHERE Id LIKE '%$id%'"
-                database.execSQL(query)
-                //checkBoxesStates.put(pos, false)
-                //println(position)
-                holder.finalCheck.isChecked = false
-                holder.finalCheck.text = "✖ not completed"
-                holder.finalCheck.setBackgroundColor(Color.parseColor("#FF0000"))
-                val toast = Toast.makeText(context, negativeMessage, Toast.LENGTH_SHORT)
-                toast.show()
-                database.close()
+        when (holder.align) {
+            "CENTER" -> {
+                holder.noteContent.gravity = Gravity.CENTER
             }
-            //println(checkBoxesStates)
+            "LEFT" -> {
+                holder.noteContent.gravity = Gravity.LEFT
+            }
+            "RIGHT" -> {
+                holder.noteContent.gravity = Gravity.RIGHT
+            }
         }
 
-        holder.finalCheck.setOnCheckedChangeListener { _, isChecked ->
-            //list.checkedValue = isChecked.toString()
-            //updateList(dataList)
-            checkboxEvent()
+        when (holder.font1) {
+            "MONOSPACE" -> {
+                holder.noteName.typeface = Typeface.MONOSPACE
+            }
+            "SERIF" -> {
+                holder.noteName.typeface = Typeface.SERIF
+            }
+            "SANS-SERIF" -> {
+                holder.noteName.typeface = Typeface.SANS_SERIF
+            }
+            else -> {
+                holder.noteName.typeface = Typeface.SERIF
+            }
+        }
+
+        when (holder.font2) {
+            "MONOSPACE" -> {
+                holder.noteContent.typeface = Typeface.MONOSPACE
+            }
+            "SERIF" -> {
+                holder.noteContent.typeface = Typeface.SERIF
+            }
+            "SANS-SERIF" -> {
+                holder.noteContent.typeface = Typeface.SANS_SERIF
+            }
+            else -> {
+                holder.noteContent.typeface = Typeface.SERIF
+            }
         }
 
         fun viewNote() {
@@ -276,24 +181,34 @@ class RecyclerViewAdapter(val context:Context, var dataList:ArrayList<NoteData>)
             editName.setText(nameofNote, TextView.BufferType.EDITABLE)
             editContent.setText(contentofNote, TextView.BufferType.EDITABLE)
 
-            if (holder.font1 == "SERIF") {
-                editName.typeface = Typeface.SERIF
-            } else if (holder.font1 == "MONOSPACE") {
-                editName.typeface = Typeface.MONOSPACE
-            } else if (holder.font1 == "SANS-SERIF") {
-                editName.typeface = Typeface.SANS_SERIF
-            } else {
-                editName.typeface = Typeface.SERIF
+            when (holder.font1) {
+                "SERIF" -> {
+                    editName.typeface = Typeface.SERIF
+                }
+                "MONOSPACE" -> {
+                    editName.typeface = Typeface.MONOSPACE
+                }
+                "SANS-SERIF" -> {
+                    editName.typeface = Typeface.SANS_SERIF
+                }
+                else -> {
+                    editName.typeface = Typeface.SERIF
+                }
             }
 
-            if (holder.font2 == "SERIF") {
-                editContent.typeface = Typeface.SERIF
-            } else if (holder.font2 == "MONOSPACE") {
-                editContent.typeface = Typeface.MONOSPACE
-            } else if (holder.font2 == "SANS-SERIF") {
-                editContent.typeface = Typeface.SANS_SERIF
-            } else {
-                editContent.typeface = Typeface.SERIF
+            when (holder.font2) {
+                "SERIF" -> {
+                    editContent.typeface = Typeface.SERIF
+                }
+                "MONOSPACE" -> {
+                    editContent.typeface = Typeface.MONOSPACE
+                }
+                "SANS-SERIF" -> {
+                    editContent.typeface = Typeface.SANS_SERIF
+                }
+                else -> {
+                    editContent.typeface = Typeface.SERIF
+                }
             }
 
             builder.setView(dialog)
@@ -325,7 +240,7 @@ class RecyclerViewAdapter(val context:Context, var dataList:ArrayList<NoteData>)
 
         fun copyNote() {
             val clipboardManager = context.getSystemService(CLIPBOARD_SERVICE) as ClipboardManager
-            var clipData: ClipData
+            val clipData: ClipData
 
             val contentToCopy = holder.noteContent.text
 
@@ -358,12 +273,5 @@ class RecyclerViewAdapter(val context:Context, var dataList:ArrayList<NoteData>)
     fun updateData(newList: ArrayList<NoteData>) {
         dataList = newList
         notifyDataSetChanged()
-    }
-
-    fun updateList(updatedList: ArrayList<NoteData>) { //update data list to keep checkboxes states
-        //updatedList.clear()
-        dataList.clear()
-        dataList = updatedList
-        //notifyDataSetChanged()
     }
 }
