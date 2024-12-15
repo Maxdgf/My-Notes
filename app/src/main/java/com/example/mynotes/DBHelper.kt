fb58c63cebb8f4384e05ed034ca78e2d527f83de
Max <162334963+Maxdgf@users.noreply.github.com>
2024-12-15 16:42:43 +0300

    Update DBHelper.kt

--- app/src/main/java/com/example/mynotes/DBHelper.kt
+++ app/src/main/java/com/example/mynotes/DBHelper.kt
@@ -8,22 +8,21 @@ class DBHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null
 
     companion object {
         private const val DATABASE_NAME = "NoteDataBox.db"
-        private const val DATABASE_VERSION = 7
+        private const val DATABASE_VERSION = 10
 
         private const val TABLE_NAME = "NotesContentTable"
         private const val ID = "Id"
         private const val NOTE_NAME = "NoteName"
         private const val NOTE_CONTENT = "NoteContent"
-        private const val IS_CHECKED = "IsChecked"
-        private const val CHECKED_VALUE = "CheckValue"
         private const val DATE_TODAY = "DateNow"
         private const val PLACE = "Place"
         private const val NAME_FONT = "NameFont"
         private const val CONTENT_FONT = "ContentFont"
+        private const val TEXT_ALIGN = "TextAlign"
     }
 
     override fun onCreate(db: SQLiteDatabase?) {
-        val createTableQuery = "CREATE TABLE $TABLE_NAME ($ID INTEGER, $NOTE_NAME TEXT, $NOTE_CONTENT TEXT, $IS_CHECKED TEXT, $CHECKED_VALUE TEXT, $DATE_TODAY TEXT, $PLACE INTEGER, $NAME_FONT TEXT, $CONTENT_FONT TEXT)"
+        val createTableQuery = "CREATE TABLE $TABLE_NAME ($ID INTEGER, $NOTE_NAME TEXT, $NOTE_CONTENT TEXT, $DATE_TODAY TEXT, $PLACE INTEGER, $NAME_FONT TEXT, $CONTENT_FONT TEXT, $TEXT_ALIGN TEXT)"
         db?.execSQL(createTableQuery)
     }
 
